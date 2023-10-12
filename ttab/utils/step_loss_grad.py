f8ead84c1909848c77f09cd0ac65a6632e020eb1
DYW77 <DongyanWei7@outlook.com>
2023-10-12 09:43:46 +0800

    "fix csv file path"

--- ttab/utils/step_loss_grad.py
+++ ttab/utils/step_loss_grad.py
@@ -59,7 +59,7 @@ def saveAsCSV(conf: Any , state , batch):
             'loss_grad',
             # f"{conf.model_name}_{conf.base_data_name}_{conf.model_adaptation_method}_{conf.model_selection_method}_{int(conf.timestamp if conf.timestamp is not None else time.time())}-seed{conf.seed}",
             # f"{conf.model_name}_{conf.base_data_name}_{conf.model_adaptation_method}_{conf.model_selection_method}_{str(time.time()).replace('.', '_')}-seed{conf.seed}",
-            f"{conf.job_id}_{conf.model_name}_{conf.base_data_name}_{conf.model_adaptation_method}_{conf.inter_domain}_{conf.corruption_num}",
+            # f"{conf.job_id}_{conf.model_name}_{conf.base_data_name}_{conf.model_adaptation_method}_{conf.inter_domain}_{conf.corruption_num}",
             # f"{conf.job_id}_{conf.model_name}_{conf.base_data_name}_{conf.model_adaptation_method}_{conf.inter_domain}",
             )
     if not os.path.exists(conf.grad_dir):
@@ -121,7 +121,7 @@ def saveAsCSV(conf: Any , state , batch):
     #+ list(temp["grads"].keys())
     DomainNum = np.zeros(15)
     Combine = None
-    csv_file = os.path.join(conf.grad_dir, f'save.csv')
+    csv_file = os.path.join(conf.grad_dir, f"{conf.job_id}_{conf.model_name}_{conf.base_data_name}_{conf.model_adaptation_method}_{conf.inter_domain}_{conf.corruption_num}.csv")
   
     if not os.path.exists(csv_file):
         with open(csv_file, 'w', newline='') as f:
